3b7c90bcb625b789ed20bc83e7f29e1e433d42d7
Ian Herrera <iantherrera@gmail.com>
2022-06-28 15:07:44 -0400

    Added portfolio design images to Portfolio page Added lightbox modal functionality with new js file & css file Changed portfolio section order & layout Cleaned up redundant css styles for elements Edited classes for more consistent styling between pages Edited ids for Portfoliio page specific styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,10 @@
+// Hamburger menu
+// Reference hamburger menu icon
 const hamburger = document.getElementById('hamburger-menu');
+// Reference nav elements
 const navMenu = document.getElementsByTagName('nav')[0];
-const section = document.getElementsByTagName('section')[0];
 
-
+// Hamburger menu click triggers nav height resize & applies nav styles & transformation animations
 hamburger.addEventListener('click', () => {
   if (navMenu.style.height == 0 || navMenu.style.height == '0px') {
     hamburger.classList.add("open");
@@ -13,16 +15,60 @@ hamburger.addEventListener('click', () => {
   }
 });
 
+// Reset hamburger menu states when resizing
 window.addEventListener('resize', () => {
   if (window.innerWidth > 599) {
     navMenu.style.height = 'unset';
+    hamburger.classList.remove('open');
   } else if (window.innerWidth < 600 && navMenu.style.height !== 0) {
     navMenu.style.height = 0;
   }
 });
+//Hamburger menu
 
+
+//Scrollbar visibility
+// Wait for section animations to end for scrollbar to appear
+// Reference section elements
+const section = document.getElementsByTagName('section')[0];
+
+// Add scrollbar if needed after animations
 section.addEventListener('animationend', () => {
   document.body.style.overflowY = "auto";
 });
+//Scrollbar visibility
 
+//Footer
+// Add year to copyright in footer
 document.querySelector('#year').innerText = new Date().getFullYear();
+//Footer
+
+
+// Lightbox modals
+// Modal reference
+var modal = document.getElementById('myModal');
+// Reference images by class
+var images = document.getElementsByClassName('myImages');
+// Reference image in modal
+var modalImg = document.getElementById("img01");
+// Reference modal caption
+var captionText = document.getElementById("caption");
+
+// Loop through class images
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // Add click listener for image
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+// Modal close functionality
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// Lightbox modals
