a20bb5069a28651be06826c2d2d02f1c012b19bc
Ian Herrera <iantherrera@gmail.com>
2022-07-25 02:03:07 -0400

    Fixed pathing to image gallery Fixed hover state and styling of gitlink

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,90 +1,90 @@
 // Design gallery array of images and attributes
 const images = [
     {
-        src: "../Portfolio/images/design/decca_flyer_2015.jpg",
+        src: "../images/design/decca_flyer_2015.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Short Film Festival Poster - 2015"
     },
 
     {
-        src: "../Portfolio/images/design/uncorked_poster_2015.jpg",
+        src: "../images/design/uncorked_poster_2015.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Short Film Festival Poster - 2015"
     },
     {
-        src: "../Portfolio/images/design/prettygoslings_promo_2015.jpg",
+        src: "../images/design/prettygoslings_promo_2015.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Short Film Festival Poster - 2015"
     },
 
     {
-        src: "../Portfolio/images/design/wesfest_poster_2015.jpg",
+        src: "../images/design/wesfest_poster_2015.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Multimedia Festival Event Poster - 2015"
     },
 
     {
-        src: "../Portfolio/images/design/wesfest_poster_2016.jpg",
+        src: "../images/design/wesfest_poster_2016.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Multimedia Festival Event Poster - 2016"
     },
 
     {
-        src: "../Portfolio/images/design/wesfest_subpromo_2016.jpg",
+        src: "../images/design/wesfest_subpromo_2016.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Multimedia Festival Event Poster - 2016"
     },
 
     {
-        src: "../Portfolio/images/design/moulin_louge_poster_2016.jpg",
+        src: "../images/design/moulin_louge_poster_2016.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Multimedia Festival Event Poster - 2016"
     },
 
     {
-        src: "../Portfolio/images/design/reconstruction_logo.jpg",
+        src: "../images/design/reconstruction_logo.jpg",
         class: "myImages",
         id: "myImg",
         alt: "REConstruction LLC Logo design"
     },
 
     {
-        src: "../Portfolio/images/design/gattaca_poster.jpg",
+        src: "../images/design/gattaca_poster.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Experimental Poster"
     },
 
     {
-        src: "../Portfolio/images/design/setmefree_poster_2017.jpg",
+        src: "../images/design/setmefree_poster_2017.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Multimedia Festival Event Poster - 2017"
     },
 
     {
-        src: "../Portfolio/images/design/feva_modernad_2017.jpg",
+        src: "../images/design/feva_modernad_2017.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Two-page Promotional Print Ad - 2017"
     },
 
     {
-        src: "../Portfolio/images/design/feva_prideguide_2018.jpg",
+        src: "../images/design/feva_prideguide_2018.jpg",
         class: "myImages",
         id: "myImg",
         alt: "Two-page Event Promotional Print Ad - 2018"
     },
 
     {
-        src: "../Portfolio/images/design/chewie_forceyall_tshirt.jpg",
+        src: "../images/design/chewie_forceyall_tshirt.jpg",
         class: "myImages",
         id: "myImg",
         alt: "T-Shirt Design"
